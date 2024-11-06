6a2e5afc08e054f8fffab1c0ca9b42fdebe99caf
1999Snehal <snehallagad84@gmail.com>
2024-11-06 13:04:48 +0530

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+
+class MyThread implements Runnable
+{
+ public void run()
+ {
+     for(int i = 1; i<=10; i++)
+     {
+         System.out.println("Value of i is:" +i);
+
+         try
+         {
+             Thread.sleep(1000);
+         }
+         catch (Exception e)
+         {
+          }
+ }
 }
+    public static void main(String[] args)
+    {
+        MyThread t1 = new MyThread();
+
+            Thread thr = new Thread(t1);
+
+            thr.start();
+
+        }
+    }
